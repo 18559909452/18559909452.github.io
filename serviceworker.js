2b3446d845f19751f9af3653ea2845f479763b5f
junfengchen <18559909452@users.noreply.github.com>
2018-06-06 19:38:40 +0800

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -9,12 +9,35 @@ function updateStaticCache() {
   return caches.open(version + staticCacheName)
     .then(function (cache) {
       return cache.addAll([
-        '/path/to/javascript.js',
-        '/path/to/stylesheet.css',
-        '/path/to/someimage.png',
-        '/path/to/someotherimage.png',
-        '/',
-        '/offline'
+        '/ubuild.dll.e83e29bc75b60fbb2e3a.js'
       ]);
     });
 };
+
+
+
+this.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                console.log('cache:' + event.request);
+                return response;
+            }
+
+            var request = event.request.clone();
+            return fetch(request).then(function (httpRes) {
+                if (!httpRes || httpRes.status !== 200) {
+                    return httpRes;
+                }
+
+                // 请求成功的话，将请求缓存起来。
+                var responseClone = httpRes.clone();
+                caches.open(version + staticCacheName).then(function (cache) {
+                    cache.put(event.request, responseClone);
+                });
+
+                return httpRes;
+            });
+        })
+    );
+});
